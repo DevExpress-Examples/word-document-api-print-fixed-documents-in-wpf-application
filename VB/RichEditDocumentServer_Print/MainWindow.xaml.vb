568cfe84c5a8b97cb83a91f17a6189048d1616b3
DevExpressExampleBot <fakeEmail>
2021-09-02 19:25:51 +0300

    Source auto update [skip ci]

--- VB/RichEditDocumentServer_Print/MainWindow.xaml.vb
+++ VB/RichEditDocumentServer_Print/MainWindow.xaml.vb
@@ -6,40 +6,40 @@ Imports System.Windows.Controls
 Imports System.Windows.Documents
 
 Namespace RichEditDocumentServer_Print
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-'            #Region "#xpfprinter"
-            Dim srv As New RichEditDocumentServer()
-            srv.LoadDocument("test.docx")
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+'			#Region "#xpfprinter"
+			Dim srv As New RichEditDocumentServer()
+			srv.LoadDocument("test.docx")
 
-            Dim document As FixedDocument = RichEditDocumentXpfPrinter.CreateFixedDocument(srv)
+			Dim document As FixedDocument = RichEditDocumentXpfPrinter.CreateFixedDocument(srv)
 
-            Dim pDialog As New PrintDialog()
-            Dim queues As PrintQueueCollection = (New PrintServer()).GetPrintQueues( { EnumeratedPrintQueueTypes.Local, EnumeratedPrintQueueTypes.Connections})
-            Dim localPrinterEnumerator As System.Collections.IEnumerator = queues.GetEnumerator()
-            Dim printQueue As PrintQueue = Nothing
+			Dim pDialog As New PrintDialog()
+			Dim queues As PrintQueueCollection = (New PrintServer()).GetPrintQueues( { EnumeratedPrintQueueTypes.Local, EnumeratedPrintQueueTypes.Connections})
+			Dim localPrinterEnumerator As System.Collections.IEnumerator = queues.GetEnumerator()
+			Dim printQueue As PrintQueue = Nothing
 
-            Do
-                If Not localPrinterEnumerator.MoveNext() Then
-                    Exit Do
-                End If
-                printQueue = DirectCast(localPrinterEnumerator.Current, PrintQueue)
-            Loop While Not printQueue.FullName.Contains("Canon")
-            If printQueue IsNot Nothing Then
-                pDialog.PrintQueue = printQueue
-                pDialog.PrintDocument(document.DocumentPaginator, String.Empty)
-            End If
-'            #End Region ' #xpfprinter
-        End Sub
-    End Class
+			Do
+				If Not localPrinterEnumerator.MoveNext() Then
+					Exit Do
+				End If
+				printQueue = DirectCast(localPrinterEnumerator.Current, PrintQueue)
+			Loop While Not printQueue.FullName.Contains("Canon")
+			If printQueue IsNot Nothing Then
+				pDialog.PrintQueue = printQueue
+				pDialog.PrintDocument(document.DocumentPaginator, String.Empty)
+			End If
+'			#End Region ' #xpfprinter
+		End Sub
+	End Class
 End Namespace
 
